2d16c41f31364233237eec438405c2ba3f7c54c8
Abraham Hokuto <shikieiki@yamaxandu.org>
2017-06-04 03:00:58 +0900

    red guard

--- src/stack.cpp
+++ src/stack.cpp
@@ -9,14 +9,15 @@
 suika::stack_info
 suika::stack_allocator::alloc(std::size_t stack_size)
 {
-        auto ret = mmap(nullptr, stack_size + 4096 * 2, PROT_READ | PROT_WRITE,
+        stack_size += 4096 * 2;
+        auto ret = mmap(nullptr, stack_size, PROT_READ | PROT_WRITE,
                         MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
 
         if (ret == MAP_FAILED)
                 throw std::system_error(errno, std::system_category());
 
         if (mprotect(static_cast<char*>(ret) + 4096, 4096, PROT_NONE) < 0) {
-                munmap(ret, stack_size + 4096 * 2);
+                munmap(ret, stack_size);
                 throw std::system_error(errno, std::system_category());
         }
 
@@ -29,6 +30,6 @@ void
 suika::stack_allocator::dealloc(const stack_info& info)
 {
         auto stack_size = reinterpret_cast<std::uint64_t>(info.sp) - reinterpret_cast<std::uint64_t>(info.vp);
-        if (munmap(info.vp, stack_size + 4096 * 2) < 0)
+        if (munmap(info.vp, stack_size) < 0)
                 throw std::system_error(errno, std::system_category());
 }
